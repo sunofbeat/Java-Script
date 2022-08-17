c57138c168eb4bbfd692eb5f8217334f9ebe8123
sunofbeat <sunmam964@naver.com>
2022-08-17 16:33:28 +0900

    0817

--- 3.object/home/4.jajangmyeon.js
+++ 3.object/home/4.jajangmyeon.js
@@ -1,26 +1,52 @@
 /* 과제:
 ~를 객체지향으로 refactoring하라.
-*/
 
-
-function Chef(name){
+/*
+function Food(name) {
     this.name = name
-    this.cook = food
 }
+
+function Chef1(){
+    this.cook = food => console.log(`매운 ${food}`)
+}
+
+function Chef2(){
+    this.cook = food => console.log('달콤한')
+}
+
+function Waiter(food, chef){
+    this.food = food
+    this.chef = chef
+    this.serving = (food, chef) => chef(food)
+}
+
+const food = new Food('짜장면')
+const chef1 = new Chef1()
+const chef2 = new Chef2()
+const waiter = new Waiter()
+
+waiter.serving(food.name, chef1.cook)
+ */
 
 function Food(name) {
     this.name = name
-}      
-
-function Waiter(Food){
-    this.serving = food => new Food(food)
 }
 
+function Chef1() {
+    this.cook = foodName => new Food(`달콤한 ${foodName}`)
+}
 
-const food = new Food()
-const chef1 = new Chef('달콤한')
-const chef2 = new Chef('매운')
-const waiter = new Waiter
+function Chef2() {
+    this.cook = foodName => new Food(`매운 ${foodName}`)
+}
 
-console.log(waiter.serving(chef1.cook('짜장면')))
- 
+function Waiter() {
+    this.serving = food => console.log(food)
+}
+
+const chef1 = new Chef1()
+const chef2 = new Chef2()
+const waiter = new Waiter()
+
+waiter.serving(chef1.cook('짜장면'))
+waiter.serving(chef2.cook('짜장면'))
